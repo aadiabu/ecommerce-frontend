99e881d96cc9a0abcfc34bd8b5f935dbe2ee2ef2
Adil Amin <aadiabu111@gmail.com>
2025-07-23 12:17:25 +0530

    Initial project setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
 console.log("E-Commerce Website Loaded");
+// Select elements
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// Toggle class on hamburger click
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
